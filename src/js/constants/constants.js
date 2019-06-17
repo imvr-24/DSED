d639ea93f739c4db6fab7b39e5387799d92cb75f
imvr-24 <veerareddy596@gmail.com>
2019-06-17 15:52:40 +0530

    updated project structure

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,7 +1,7 @@
 
 const API_KEY = 'ddc2c672ba85e86fb279c29bb685edd1';
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
-const BASE_URL_FORECAST = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const BASE_URL_FORECAST = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 
 export const config = {
     apiKey: API_KEY,
